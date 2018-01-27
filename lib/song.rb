75c572a04a08a99869155ed46e26796a8038e391
Kathy Hudson <khitx@icloud.com>
2018-01-27 18:08:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,16 @@ class Song
     @@song_cnt = 0
     @@artists = []
     @@genres = []
+    @@genre_count = {}
 
     def initialize(name, artist, genre)
-        @@song_cnt += 1
-        @@artists << artist
-        @@genres << genre
         @name = name
         @artist = artist
         @genre = genre
+        @@song_cnt += 1
+        @@artists << artist
+        @@genres << genre
+        @@genre_count.each { |g, cnt| g == genre ? cnt += 1 }
     end
 
     def self.count
