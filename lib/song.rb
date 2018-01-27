6b43f27109c8d1801d853b1432ff9aaa237ec7d1
Kathy Hudson <khitx@icloud.com>
2018-01-27 17:55:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ class Song
     attr_accessor :name, :artist, :genre
     @@song_cnt = 0
     @@artists = []
+    @@genres = []
 
     def initialize(name, artist, genre)
         @@song_cnt += 1
         @@artists << artist
+        @@genres << genre
         @name = name
         @artist = artist
         @genre = genre
@@ -19,5 +21,8 @@ class Song
       @@artists
     end
 
-    
+    def self.genres
+      @@genres
+    end
+
 end
